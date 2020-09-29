0abc1193aaf6b144e36a08f62d8ba20c0e0b4b37
1164715233 <71438243+1164715233@users.noreply.github.com>
2020-09-29 04:46:15 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+rm -Rf files/etc/profile.d/sysinfo.sh
+find target/linux/ramips/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}' \;
+echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
